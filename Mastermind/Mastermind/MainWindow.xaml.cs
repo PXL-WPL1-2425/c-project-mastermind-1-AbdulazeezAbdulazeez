e5d059785f8789b99d067ed190cf6626443e82bd
AbdulazeezAbdulazeez <12402938@student.pxl.be>
2024-11-04 15:11:57 +0100

    MasterMind-03

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -1,31 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Mastermind
 {
     public partial class MainWindow : Window
     {
         List<string> kleuren = new List<string> { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
-        List<string> Random = new List<string>();
+        List<string> gegenereerdeCode = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
-            RandomKleur();
+            RandomKleur();  
+            ComboBoxes(); 
         }
 
         private void RandomKleur()
         {
             var random = new Random();
-            Random.Clear();
+            gegenereerdeCode.Clear();
 
             for (int i = 0; i < 4; i++)
             {
-                Random.Add(kleuren[random.Next(kleuren.Count)]);
+                gegenereerdeCode.Add(kleuren[random.Next(kleuren.Count)]);
             }
 
-            Title = $"MasterMind ({string.Join(", ", Random)})";
+                Title = $"MasterMind ({string.Join(", ", gegenereerdeCode)})";
+        }
+
+        private void ComboBoxes()
+        {
+            ComboBox1.ItemsSource = kleuren;
+            ComboBox2.ItemsSource = kleuren;
+            ComboBox3.ItemsSource = kleuren;
+            ComboBox4.ItemsSource = kleuren;
         }
     }
 }
