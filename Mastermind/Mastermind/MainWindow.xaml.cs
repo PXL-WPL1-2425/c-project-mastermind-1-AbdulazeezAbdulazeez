8e613ff9326dd1ebca654976958db319e3edb7b7
AbdulazeezAbdulazeez <12402938@student.pxl.be>
2024-11-04 15:46:28 +0100

    MasterMind-04

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -2,32 +2,33 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Mastermind
 {
     public partial class MainWindow : Window
     {
         List<string> kleuren = new List<string> { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
-        List<string> gegenereerdeCode = new List<string>();
+        List<string> Random = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
-            RandomKleur();  
-            ComboBoxes(); 
+            RandomKleur();
+            ComboBoxes();
         }
 
         private void RandomKleur()
         {
             var random = new Random();
-            gegenereerdeCode.Clear();
+            Random.Clear();
 
             for (int i = 0; i < 4; i++)
             {
-                gegenereerdeCode.Add(kleuren[random.Next(kleuren.Count)]);
+                Random.Add(kleuren[random.Next(kleuren.Count)]);
             }
 
-                Title = $"MasterMind ({string.Join(", ", gegenereerdeCode)})";
+            Title = $"MasterMind ({string.Join(", ", Random)})";
         }
 
         private void ComboBoxes()
@@ -36,6 +37,38 @@ namespace Mastermind
             ComboBox2.ItemsSource = kleuren;
             ComboBox3.ItemsSource = kleuren;
             ComboBox4.ItemsSource = kleuren;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+
+            if (comboBox.SelectedItem != null)
+            {
+                string selectedColor = comboBox.SelectedItem.ToString();
+                Brush brushColor = (Brush)new BrushConverter().ConvertFromString(selectedColor);
+
+                switch (comboBox.Name)
+                {
+                    case "ComboBox1":
+                        Label1.Background = brushColor;
+                        break;
+                    case "ComboBox2":
+                        Label2.Background = brushColor;
+                        break;
+                    case "ComboBox3":
+                        Label3.Background = brushColor;
+                        break;
+                    case "ComboBox4":
+                        Label4.Background = brushColor;
+                        break;
+                }
+            }
+        }
+
+        private void CheckCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
