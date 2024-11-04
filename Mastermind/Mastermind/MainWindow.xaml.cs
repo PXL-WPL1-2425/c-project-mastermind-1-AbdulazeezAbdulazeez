8f310e524d55980fcc64c1ba2f31d16a5f9413bb
AbdulazeezAbdulazeez <12402938@student.pxl.be>
2024-11-04 14:58:37 +0100

    Mastermind-02

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -1,24 +1,31 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Mastermind
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> kleuren = new List<string> { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+        List<string> Random = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            RandomKleur();
+        }
+
+        private void RandomKleur()
+        {
+            var random = new Random();
+            Random.Clear();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Random.Add(kleuren[random.Next(kleuren.Count)]);
+            }
+
+            Title = $"MasterMind ({string.Join(", ", Random)})";
         }
     }
 }
